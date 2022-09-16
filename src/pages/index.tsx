0ea4a22a6f5cdd76d556890d27d4289fa75461b4
nascjoao <oi@joaonasc.dev>
2022-09-16 11:47:27 -0300

    starting project

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Rock Paper Scissors</title>
+        <meta name="description" content="Rock Paper Scissors Game!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default Home
