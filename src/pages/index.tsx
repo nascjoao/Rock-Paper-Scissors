62b21d8a28a0950859762b0e9c8ed666c8b471d6
nascjoao <oi@joaonasc.dev>
2022-09-16 20:36:36 -0300

    refactor: head description and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,9 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Rock Paper Scissors</title>
-        <meta name="description" content="Rock Paper Scissors Game!" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Rock Paper Scissors - Jogo de Pedra Papel Tesoura!" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✊</text></svg>" />
       </Head>
-
       <Game />
     </>
   )
